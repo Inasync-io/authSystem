b240bc9074a6f2ff5e040e2c16a81235312b633f
HustlrDen <nrogith@yahoo.com>
2025-05-21 16:04:05 +0530

    update the route login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,37 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  
+  const [isAthu, setIsAuth] = useState(false);
+
   useEffect(() => {
-    const token = localStorage.getItem('auth_token');
+    const token = localStorage.getItem("auth_token");
 
     if (!token) {
-      router.replace('/login');
+      router.replace("/login");
+    } else {
+      setIsAuth(true);
     }
   }, [router]);
 
+  if (!isAthu) return null;
+
+  const handleLogout = () => {
+    localStorage.removeItem("auth_token");
+    router.replace("/login");
+  };
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold font-poppins">Hello, world!</h1>
+      <button
+        className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
     </main>
   );
 }
